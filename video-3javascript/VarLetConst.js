fe0c0878dadae0bd4d2d36b07ea1b139e0d181a3
Nihal soni <nihalsoni0523@gmail.com>
2025-02-28 00:11:05 +0530

    update

---
+++ video-3javascript/VarLetConst.js
@@ -0,0 +1,30 @@
+// VAR LET CONST KI KAHANI 
+
+var a = 23;
+var b = 56;
+a = "nihal";
+const ans = a + b;
+console.log(ans);
+
+
+// Var can be changed
+// const can not be changed
+
+// lets take another example
+
+const firstName = "Nihal";
+const lastName = "soni";
+
+const fullName = firstName + lastName;
+console.log(fullName)
+
+// now what if i want to change the value of const lets try doing isJSDocThisTag;
+
+const firstName2 = "Nihal";
+const lastName2 = "soni";
+
+firstName2 = "Ameen"
+
+const fullName2 = firstName + lastName;
+console.log(fullName)
+
